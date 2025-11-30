45fbcd20ba333a4e42287dccebd6bc74408427fb
belaleid5 <belaleid788@gmail.com>
2025-11-30 23:15:26 +0200

    Done Font family && font  weight &&  textStyle

---
+++ lib/core/routes/routes_names.dart
@@ -0,0 +1,17 @@
+class AppRoutesNames{
+    static const splashRoute = '/splash';
+  static const loginRoute = '/login';
+  static const signUpRoute = '/signin';
+  static const forgetPasswordRoute = "/forgetPassword";
+  static const mainRoute = "/mainRoute";
+  static const homeRoute = "/homeRoute";
+
+  static const searchRoute = "/search";
+  static const filterSearchRoute = "/filterSearch";
+  static const verifyPhoneRoute = "/verifyPhone";
+  static const verifyConfirmPasswordRoute = "/veriyConfirmPasswordRoute";
+  static const otpRoute = "/otp";
+  static const onBoarding = "/onBoard";
+  static const carDetilesHomeRoute = "/carDetilesHomeRoute";
+
+}
