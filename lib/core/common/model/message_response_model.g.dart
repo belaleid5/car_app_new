fd2273ffe29c9cca0645419200972c5d090fb82c
belaleid5 <belaleid788@gmail.com>
2026-02-10 22:45:15 +0200

    update the  coding

---
+++ lib/core/common/model/message_response_model.g.dart
@@ -0,0 +1,15 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'message_response_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_MessageResponseModel _$MessageResponseModelFromJson(
+  Map<String, dynamic> json,
+) => _MessageResponseModel(message: json['message'] as String);
+
+Map<String, dynamic> _$MessageResponseModelToJson(
+  _MessageResponseModel instance,
+) => <String, dynamic>{'message': instance.message};
